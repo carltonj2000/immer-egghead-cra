593ae823b8a67f6f45f863e2ac67018ee3b3a0ad
Carlton Joseph <carlton.joseph@gmail.com>
2021-04-23 20:18:45 -0700

    add non-immer and gui gift reservation and tests

---
+++ src/gifts.js
@@ -0,0 +1,24 @@
+export const addGift = (state, id, description, image) => {
+  return {
+    ...state,
+    gifts: [...state.gifts, { id, description, image, reservedBy: undefined }],
+  };
+};
+
+export const toggleReservation = (state, giftId) => {
+  return {
+    ...state,
+    gifts: state.gifts.map((gift) => {
+      if (gift.id !== giftId) return gift;
+      return {
+        ...gift,
+        reservedBy:
+          gift.reservedBy === undefined
+            ? state.currentUser.id
+            : gift.reservedBy === state.currentUser.id
+            ? undefined
+            : gift.reservedBy,
+      };
+    }),
+  };
+};
