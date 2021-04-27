b8b29891d7a7214a99332a48c6c4bb3e5106e251
Carlton Joseph <carlton.joseph@gmail.com>
2021-04-26 18:09:32 -0700

    compress patches

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const WebSocket = require("ws");
+const gifts = require("./src/misc/gifts.json");
+const immer = require("immer");
+
+const { produceWithPatches, applyPatches, enablePatches } = immer;
+
+enablePatches();
+
+const initialState = { gifts };
 
 const wss = new WebSocket.Server({ port: 5001 });
 
 const connections = [];
 
-const history = [];
+let history = [];
 
 wss.on("connection", (ws) => {
   console.log("New connection!");
@@ -27,3 +35,15 @@ wss.on("connection", (ws) => {
 
   ws.send(JSON.stringify(history));
 });
+
+function compressHistory(currentPatches) {
+  const [, patches] = produceWithPatches(initialState, (draft) => {
+    return applyPatches(draft, currentPatches);
+  });
+  console.log(`compressed from ${currentPatches.length} to ${patches.length}`);
+  return patches;
+}
+
+setInterval(() => {
+  history = compressHistory(history);
+}, 5000);
